1a9e335fe183659a3a7cb6e6f13ae83b8db0956a
fabio <felipesilvalima200@gmail.com>
2024-08-01 16:25:42 -0300

    Conversor de Moedas

--- card.php
+++ card.php
@@ -3,25 +3,75 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio 3</title>
+    <title>Conversor de moedas</title>
     <link rel="stylesheet" href="styles3.css">
 </head>
 <body>
     <main>
         <h1>Conversor de Moedas</h1>
 <?php 
-$dolar= $_GET["real"];
-$real = $dolar/5.6472;
-$calcu = round($real,2);
 
+$inicio=date("m-d-Y", strtotime("-7 days"));
+$fim=date("m-d-Y");
+$url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
-echo"Seus R$ $dolar equivalem a<strong> US$ $calcu</strong>";
-echo"<br><strong>Cotação fixa de R$5,61</strong> informada diretamente no código.";
+$dados = json_decode(file_get_contents($url), true);
+$cotaçao =$dados["value"][0]["cotacaoCompra"];
+ 
+$euro = 6.1743;  
+$libra = 7.2926	; 
+$lene = 0.03823;  
+$franco = 6.5528;  
+$peso = 0.0061;
+$rublo = 0.067;
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $valorReal = isset($_GET['valor']) ? floatval($_GET['valor']) : 0;
+    $moedaSelecionada = isset($_GET['moeda']) ? $_GET['moeda'] : '';
+
+    $resultado = 0;
+
+    // Exemplo de ação baseada na opção selecionada
+    switch ($moedaSelecionada) {
+        case 'dolar':
+            $resultado =  $valorReal/$cotaçao;
+            break;
+        case 'euros':
+            $resultado =  $valorReal/$euro;;
+            break;
+        case 'libras':
+            $resultado =  $valorReal/$libra;
+            break;
+        case 'lenes':
+            $resultado =  $valorReal/$lene;
+            break;
+        case 'francos':
+            $resultado =  $valorReal/$franco;
+            break;
+        case 'pesos':
+            $resultado =  $valorReal/$peso;
+            break;
+        case 'rublos':
+             $resultado =  $valorReal/$rublo;
+            break;
+    }
+    $resultfinal = number_format($resultado,2,",",".");
+
+    echo "<p>Valor em reais: R$ $valorReal</p>";
+    echo "<p>Moeda selecionada: $moedaSelecionada</p>";
+    echo "<p>Resultado da conversão:<strong>$resultfinal</strong></p>";
+} else {
+    echo "Método de requisição inválido.";
+} 
 ?>
+
 <br />
-<br />
-<button onclick="javascript:history.go(-1)">&#11013; Voltar</button>
+<button onclick="javascript:window.location.href='index.html'">&#11013; Voltar</button>
+
 
     </main>
 </body>
 </html>
+
+
